1aaedf79a8faac50b4879d25b14a35e1d8c28d75
yelenita14 <anchundiakaren005@gmail.com>
2026-01-17 10:00:35 -0500

    Proyecto Final

--- CONTROL ASISTENCIAS - ULEAM/src/router/index.js
+++ CONTROL ASISTENCIAS - ULEAM/src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useAuthStore } from '@/stores/auth'
 import Login from '@/views/Login.vue'
 import Dashboard from '@/views/Dashboard.vue'
 import Estudiantes from '@/views/Estudiantes.vue'
@@ -10,18 +11,30 @@ import PanelEstudiante from '@/views/PanelEstudiante.vue'
 
 const routes = [
   {path: '/', name: 'Login', component: Login},
-  {path : '/dashboard', name: 'Dashboard', component: Dashboard},
-  {path : '/estudiantes', name: 'Estudiantes', component: Estudiantes},
-  {path : '/asistencias', name: 'Asistencias', component: Asistencias},
-  {path : '/materias', name: 'Materias', component: Materias},
-  {path : '/reportes', name: 'Reportes', component: Reportes},
+  {path : '/dashboard', name: 'Dashboard', component: Dashboard, meta: { requiresAuth: true }},
+  {path : '/estudiantes', name: 'Estudiantes', component: Estudiantes, meta: { requiresAuth: true }},
+  {path : '/asistencias', name: 'Asistencias', component: Asistencias, meta: { requiresAuth: true }},
+  {path : '/materias', name: 'Materias', component: Materias, meta: { requiresAuth: true }},
+  {path : '/reportes', name: 'Reportes', component: Reportes, meta: { requiresAuth: true }},
   {path : '/recuperar-contrasena', name: 'RecuperarContrasena', component: RecuperarContrasena},
-  {path : '/panel-estudiante', name: 'PanelEstudiante', component: PanelEstudiante},
+  {path : '/panel-estudiante', name: 'PanelEstudiante', component: PanelEstudiante, meta: { requiresAuth: true }},
 ]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes
+})
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore()
+  if (to.meta.requiresAuth && !authStore.usuarioActual){
+    next('/' )
+  }
+  else if(to.path === '/' && authStore.usuarioActual){
+    next('/dashboard')
+  }
+  else{
+    next()
+  }
 })
 
 export default router
